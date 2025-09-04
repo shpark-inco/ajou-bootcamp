12652d689f7a74226fcf0e1d18c31d1ba8a0c905
shpark-inco <shpark-inco@insilicogen.com>
2025-09-05 08:14:25 +0900

    install vim

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,7 @@
 printf "export PS1='\\[\\e[3;36m\\]\${PWD#/workspaces/} ->\\[\\e[0m\\] '\n" >> $HOME/.bashrc
 export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
 
-# Update Nextflow
-nextflow self-update
-nextflow -version
+# Install vim
+apt-get update && apt-get install -y vim
 
 cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
