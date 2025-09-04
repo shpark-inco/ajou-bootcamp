d038ae2d4bce789069e08e1dfe8ad482fab11ebf
shpark-inco <shpark-inco@insilicogen.com>
2025-09-05 07:49:06 +0900

    put files in directory

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,4 +8,9 @@ export PS1='\[\e[3;36m\]${PWD#/workspaces/} ->\[\e[0m\] '
 nextflow self-update
 nextflow -version
 
+# Install Q CLI
+curl -Lo q https://github.com/aws/q-cli/releases/latest/download/q-linux-x64
+chmod +x q
+sudo mv q /usr/local/bin/
+
 cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
